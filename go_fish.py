80e3b4f677687a75f705f400f61b26d3a8291d33
chris-jin97 <jindi@umich.edu>
2019-09-20 14:01:25 -0400

    Modify the rest of program

--- go_fish.py
+++ go_fish.py
@@ -50,7 +50,8 @@ def get_next_player(pindex):
 	return (pindex+1)%USER_NUM
 
 # keep asking until get valid input
-def ask_input(pindex):
+# parameter: pcard: the in hand cards for current player
+def ask_input(pindex,pcards):
 	while(1):
 		queface_str = input('\nHi,{}. Please choose a card rank you would like to ask the other player if they have (between 1-13):\n'.format(USER[pindex]))
 		if queface_str == 'exit':
@@ -64,33 +65,76 @@ def ask_input(pindex):
 		except:
 			print('invalid input!')
 
-def deal_with_query(player, queface):
-	pass
+def deal_with_query(pindex, hand_que, hand_ans, queface, deck):
+	if queface in [each.rank_num for each in hand_ans.cards]:
+		hand_over_cards = hand_ans.hand_over(queface)
+		for each in hand_over_cards:
+			hand_que.add_card(each)
+		next_turn = get_next_player(pindex)
+	else:
+		print("go fish!")
+		drew_card = deck.pop_card()
+		print("you drew card: {}".format(str(drew_card)))
+		hand_que.add_card(drew_card)
+		if drew_card.rank_num == queface:
+			next_turn = pindex
+		else:
+			next_turn = get_next_player(pindex)
+	return next_turn
 
-def uodate_status():
-	pass
+def has_books(hand):
+	books = []
+	cards = hand.cards
+	all_nums = [each.rank_num for each in cards]
+	for i in range(1,14):
+		if all_nums.count(i)==4:
+			books.append(i)
+	return books
 
-def is_win(all_books):
-	pass
+def update_status(bookstatus, hand_index, hand):
+	the_books = has_books(hand)
+	if len(the_books)>0:
+		bookstatus[hand_index].extend(the_books)
+		hand.remove_books(the_books)
 
-def start_turn(pindex,my_deck,plyaer_list,bookstatus):
+def is_win(hand_list,bookstatus,deck):
+	flag = 0
+	# print(len(hand_list[0].cards),'  ',len(hand_list[1].cards))
+	for each in hand_list:
+		if len(each.cards)==0:
+			flag = 1
+	if len(deck.cards) == 0:
+		flag = 1
+	if flag==1:
+		return (bookstatus.index(max(bookstatus))+1)
+	else:
+		return 0
+
+def start_turn(pindex,my_deck,player_list,bookstatus):
 	show_game_status(my_deck,player_list,bookstatus)
 	print("+++++++++++ {}'s turn +++++++++++++".format(USER[pindex]))
 	show_cards(pindex,player_list)
-	ask_input(pindex)
+	queface = ask_input(pindex,player_list[pindex].cards)
+	return queface
 
 def play_gofish():
-	init_game()
-	while not is_win():
-		start_turn()
-		deal_with_query()
-		update_states()
-	return (winner,status)
+	my_deck,player_list,bookstatus = init_game()
+	pindex = 0
+	while not is_win(player_list,bookstatus,my_deck):
+		queface = start_turn(pindex,my_deck,player_list,bookstatus)
+		aindex = get_next_player(pindex)
+		hand_que = player_list[pindex]
+		hand_ans = player_list[aindex]
+		next_turn = deal_with_query(pindex,hand_que,hand_ans,queface,my_deck)
+		update_status(bookstatus,pindex,hand_que)
+		update_status(bookstatus,aindex,hand_ans)
+		pindex = next_turn
+
+	print("+++++++++++++++ GAME OVER +++++++++++++++")
+	print("{} wins the game with {} books".format(USER[is_win(player_list,bookstatus,my_deck)],max(bookstatus)))
 
 if __name__ == "__main__":
-	my_deck,player_list,bookstatus = init_game()
-	start_turn(0,my_deck,player_list,bookstatus)
-
+	play_gofish()
 
 
 
